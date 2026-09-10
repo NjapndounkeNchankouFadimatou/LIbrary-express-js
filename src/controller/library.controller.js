657b09c2b57aa36fe74cc73fe18322d2d438d993
NjapndounkeNchankouFadimatou <fadimatou.njapndounke@facsciences-uy1.cm>
2026-09-10 13:58:05 +0100

    feat : ajout des controller library et loan

--- src/controller/library.controller.js
+++ src/controller/library.controller.js
@@ -1,38 +1,108 @@
 import prisma from "../lib/prisma.js";
 import httpCode from "../static/httpCode.js";
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
 
 const libraryController = {
-        getAllBooks: async (req, res) => {
+    getAllBooks: async (req, res) => {
         try {
+            const { title } = req.body
 
-        } catch (error) {
+            const books = await prisma.library.findMany({
+                where: { title: title }
+            })
+
+            if (!books) {
+                return res.status(httpCode.BAD_REQUEST).json({ message: "No books in the library" })
+            }
+
+            return res.status(httpCode.OK).json({ message: "Your book", books })
+
+        }
+        catch (error) {
+            return res.status(httpCode.SERVER_ERROR).json({ message: "Erreur du server", error: error.message })
 
         }
     },
-        addBook: async (req, res) => {
+    addBook: async (req, res) => {
         try {
+            const { title, author, description } = req.body
+            if (!title || !author || !description)
+                return res.status(httpCode.BAD_REQUEST).json({ message: "All the fields are required" })
 
-        } catch (error) {
+            const titleExist = await prisma.library.findUnique({
+                where: { title }
+            })
+            if (titleExist) {
+                return res.status(httpCode.BAD_REQUEST).json({ message: "This book already exist" })
+            }
+
+            const user = await prisma.library.create({
+                data: {
+                    Lib_id: uuidv4(),
+                    title,
+                    author,
+                    description
+                }
+            })
+
+            return res.status(httpCode.OK).json({ message: "Book successfully added" })
+        }
+        catch (error) {
+            return res.status(httpCode.SERVER_ERROR).json({ message: "Erreur du server", error: error.message })
 
         }
     },
 
-        updateBook: async (req, res) => {
+    updateBook: async (req, res) => {
         try {
+            const { id } = req.params
+            const { title, author, decription } = req.body
+            if (!title || !author || !decription)
+                return res.status(httpCode.BAD_REQUEST).json({ message: "All the fields are required" })
 
-        } catch (error) {
+            const bookExist = await prisma.library.findUnique({
+                where: { id }
+            })
+            if (bookExist) {
+                return res.status(httpCode.BAD_REQUEST).json({ message: "No book found" })
+            }
+
+            const updateBook = await prisma.library.update({
+                where: { id: bookExist.id },
+                data: {
+                    title: title ?? bookExist.title,
+                    author: author ?? bookExist.author,
+                    decription: decription ?? bookExist.decription
+                }
+            })
+        }
+        catch (error) {
+            return res.status(httpCode.SERVER_ERROR).json({ message: "Erreur du server", error: error.message })
 
         }
     },
-        deleteBook: async (req, res) => {
+    deleteBook: async (req, res) => {
         try {
+            if (!title || !author || !description)
+                return res.status(httpCode.BAD_REQUEST).json({ message: "All the fields are required" })
+
+            const bookExist = await prisma.library.findUnique({
+                where: { title: title }
+            })
+            if (bookExist) {
+                return res.status(httpCode.BAD_REQUEST).json({ message: "No profile found" })
+            }
+
+            const deleteBook = await prisma.library.delete({
+                where: { Lib_id: bookExist.id },
+            })
 
         } catch (error) {
+            return res.status(httpCode.SERVER_ERROR).json({ message: "Erreur du server", error: error.message })
 
         }
-    },
+    }
 
 }
 
--- src/controller/loan.controller.js
+++ src/controller/loan.controller.js
@@ -1,30 +1,99 @@
 import prisma from "../lib/prisma.js";
 import httpCode from "../static/httpCode.js";
-import {v4 as uuidv4} from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 
 
 const loanController = {
-        addLooan: async (req, res) => {
+    addLooan: async (req, res) => {
         try {
+            const { userId, booksId } = req.body
+            if (!userId || !booksId)
+                return res.status(httpCode.BAD_REQUEST).json({ message: "Enter the desired book or author" })
 
-        } catch (error) {
+            const bookExist = await prisma.library.findUnique({
+                where: { booksId }
+            })
+            if (!bookExist) {
+                return res.status(httpCode.BAD_REQUEST).json({ message: "This book does not exist" })
+
+            }
+            const loaned = prisma.loan.findUnique({
+                where: { booksId }
+            })
+            if (loaned.status === AVAILABLE) {
+                await prisma.loan.update({
+                    where: { booksId: loaned.loan_id },
+                    data: { status: LOANED },
+                    include: {
+                        library: { select: { title: true } },
+                        user: { select: { name: true } },
+
+                    }
+                })
+            }
+
+            return res.status(httpCode.OK).json({ message: "This book is loaned", loaned })
+        }
+        catch (error) {
+            return res.status(httpCode.SERVER_ERROR).json({ message: "Erreur du server", error: error.message })
 
         }
     },
-        updateLoanStatus: async (req, res) => {
+    updateLoanStatus: async (req, res) => {
         try {
+            const { loan_id } = req.params
+            const { status } = req.body
 
-        } catch (error) {
+            const LoanExist = await prisma.loan.findUnique({
+                where: { loan_id }
+            })
+            if (!LoanExist) {
+                return res.status(httpCode.BAD_REQUEST).json({ message: "No loan" })
+            }
+
+            const updateStatus = await prisma.loan.update({
+                where: { loan_id: LoanExist.loan_id },
+                data: { status: status },
+                include: {
+                    library: { select: { title: true } }
+                }
+
+            })
+            return res.status(httpCode.OK).json({ message: "Book available" })
+        }
+        catch (error) {
+            return res.status(httpCode.SERVER_ERROR).json({ message: "Erreur du server", error: error.message })
 
         }
     },
-        loanHist: async (req, res) => {
+    loanHist: async (req, res) => {
         try {
+            const { userId, booksId } = req.body
+            const UserLoan = await prisma.users.findUnique({
+                where: { userId }
+            })
 
-        } catch (error) {
+            if (!UserLoan) {
+                return res.status(httpCode.BAD_REQUEST).json({ message: "This user does not exist" })
+            }
+
+            const loans = await prisma.loan.findMany({
+                where: { userId }
+            })
+            if (!loans) {
+                return res.status(httpCode.OK).json({ message: "No loans for this user" })
+
+            }
+
+            return res.status(httpCode.OK).json({ message: "User loan history", loans })
 
         }
-    }
+
+        catch (error) {
+                return res.status(httpCode.SERVER_ERROR).json({ message: "Erreur du server", error: error.message })
+
+            }
+        }
 }
 
 export default loanController
